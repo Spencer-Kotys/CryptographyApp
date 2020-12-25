d77e8134d69d16fe314f5fd702a977ca30eed2fe
Spencer-Kotys <spencerkotys@gmail.com>
2020-12-24 20:15:01 -0600

    Add icon code

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const { app, BrowserWindow } = require('electron')
+var path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     title: 'The Cyptography App',
+    icon: path.join(__dirname, 'icons/png/64x64.png'),
     webPreferences: {
       nodeIntegration: true
     }
